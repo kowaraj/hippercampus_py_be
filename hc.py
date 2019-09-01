6623f76e57dbcee7da913a112d88808b7093e06e
kowaraj <kowaraj4stuff@gmail.com>
2019-09-01 20:52:53 +0200

    Add ql stuff for test

--- hc.py
+++ hc.py
@@ -1,7 +1,60 @@
 from flask import Flask
 from flask import request
+from flask_graphql import GraphQLView
+
+
 
 app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# Modules
+db = SQLAlchemy(app)
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+        
+class Post(db.Model):
+    __tablename__ = 'posts'
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+
+# Schema Objects
+class PostObject(SQLAlchemyObjectType):
+    class Meta:
+        model = Post
+        interfaces = (graphene.relay.Node, )
+class UserObject(SQLAlchemyObjectType):
+   class Meta:
+       model = User
+       interfaces = (graphene.relay.Node, )
+class Query(graphene.ObjectType):
+    node = graphene.relay.Node.Field()
+    all_posts = SQLAlchemyConnectionField(PostObject)
+    all_users = SQLAlchemyConnectionField(UserObject)
+schema = graphene.Schema(query=Query)
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 @app.route('/')
 def hello_world():
@@ -19,6 +72,8 @@ def login():
 def upload():
     print(request.files)
     f = request.files.get('sampleFile')
+    f.save('./uploads/'+ f.filename)
+
     print(f)
     tags = request.form.get('tags')
     print(tags)    
@@ -27,3 +82,7 @@ def upload():
         return "upload POST"
     else:
         return "upload GET"
+
+
+if __name__ == '__main__':
+     app.run()
